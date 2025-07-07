431038894a98423fabe4a9744725993ebc3c7192
taeeun98 <xodms6360@naver.com>
2025-07-07 15:02:16 +0900

    learn-react-jsx-refactoring

--- 02-component-with-props/src/main.js
+++ 02-component-with-props/src/main.js
@@ -1,18 +1,15 @@
-import { createElement as h } from "./lib/react.js";
-import ReactDOM from "./lib/react-dom.js";
-import { PlanetPage } from "./pages/PlanetPage.js";
-import { _PlanetPage } from "./pages/_PlanetPage.js";
-import AvartarPage from "./pages/AvartarPage.js";
+import React, { createElement as h } from "./lib/react.js";
+import ReactDOM, { createRoot } from "./lib/react-dom.js";
+
+
+
 
 
 
 
 const container = document.getElementById("app");
 
-if(container){
-  const reactDomRoot = ReactDOM.createRoot(container)
-  // reactDomRoot.render(h(PlanetPage)); /* class syntax */
-  // reactDomRoot.render(h(_PlanetPage)); /* function syntax */
-
-  reactDomRoot.render(h(AvartarPage)); 
+if (container) {
+  const reactDomRoot = ReactDOM.createRoot(container);
+  reactDomRoot.render(h(AvatarPage));
 }
--- 02-component-with-props/src/pages/AvartarPage.js
+++ 02-component-with-props/src/pages/AvartarPage.js
@@ -1,7 +1,15 @@
-import AvartarList from "../components/avartar/AvartarList.js";
-import React from "../lib/react.js";
+import { createElement as h } from "../lib/react.js";
+import Avatar from "../components/avatar/Avatar.js";
 
 
-export default function AvartarPage() {
-    return React.createElement(AvartarList)
+export default function AvatarPage(){
+
+  return h(
+    'ul',
+    {className:'avatarList'},
+    h(Avatar,{name:'짱구',status:'online'}),
+    h(Avatar,{name:'원장',status:'dont-disturb'}),
+    h(Avatar,{name:'봉미선',status:'offline'}),
+    h(Avatar,{name:'유리',status:'away'}),
+  )
 }
