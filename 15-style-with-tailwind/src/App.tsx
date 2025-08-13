c92e389c682a585b4cc6b4093cf8a23e0f2e4a0b
taeeun98 <xodms6360@naver.com>
2025-08-13 15:51:16 +0900

    learn-tailwind-01

---
+++ 15-style-with-tailwind/src/App.tsx
@@ -0,0 +1,10 @@
+import "/src/styles/style.css";
+
+function App() {
+  return (
+    <div style={{display:'flex'}}>
+      <h1 className="bg-indigo-500 text-white px-4 py-2 text-shadow-lg">hello tailwind</h1>
+    </div>
+  )
+}
+export default App
